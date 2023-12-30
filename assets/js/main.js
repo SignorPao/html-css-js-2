25e1812bfacdd34336a0a634f3c1799d2697be2c
Pavel Belousov <signorpao@gmail.com>
2023-12-30 11:02:46 +0300

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Mobile menu
+/*========== Mobile menu ==========*/
 const menuBtn = document.getElementById("menu-btn"),
   navLinks = document.getElementById("nav-links"),
   menuBtnIcon = menuBtn.querySelector("i");
@@ -24,3 +24,40 @@ navLinks.addEventListener("click", () => {
 //   menuBtnIcon.setAttribute("class", "ri-menu-3-line");
 //   console.log('click body');
 // });
+
+/*========== Animate elements in viewport(Scrollreveal) ==========*/
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .about__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
